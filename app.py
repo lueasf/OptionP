d8939e0c9129d2d064b5853f3525da655c101928
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2025-01-31 00:09:00 +0100

    test vercel

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def index():
 def download_pdf():
     return send_file('static/report.pdf', as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
